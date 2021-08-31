6185cbef1741706bddb1f4872ed21cd499616d30
Azzurys <azzuryytb@gmail.com>
2021-08-31 17:04:50 +0200

    Replaced own implementation by implementation at https://github.com/mpaland/printf

---
+++ kernel/panic.cpp
@@ -0,0 +1,4 @@
+//
+// Created by azzury on 30/08/2021.
+//
+
---
+++ kernel/panic.hpp
@@ -0,0 +1,8 @@
+//
+// Created by azzury on 30/08/2021.
+//
+
+#ifndef MEGAOS_PANIC_HPP
+#define MEGAOS_PANIC_HPP
+
+#endif //MEGAOS_PANIC_HPP
